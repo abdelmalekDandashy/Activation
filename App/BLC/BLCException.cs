876e30e160358a6027d1b033037cc82b88e9b7ed
abdelmalekDandashy <dandasheh1@gmail.com>
2022-01-18 17:39:23 +0400

    first commit

---
+++ App/BLC/BLCException.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BLC;
+
+namespace BLC
+{
+public partial class BLCException : Exception
+{
+public string Code { get; set; }
+#region Constructors
+public BLCException(string i_MessageContent): base(i_MessageContent)
+{
+#region Declaration And Initialization Section.
+#endregion
+#region Body Section.
+#endregion
+}
+#endregion
+}
+}
