ffb209ffb163eb6f0fe2a58d64d1a816ffa8e0de
abdelmalekDandashy <dandasheh1@gmail.com>
2022-01-27 17:24:11 +0400

    firebase notification API

---
+++ App/push-notifications-API/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using net_core_api_push_notification_demo.Models;
+using net_core_api_push_notification_demo.Services;
+
+namespace net_core_api_push_notification_demo.Controllers
+{
+    [Route("api/notification")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [Route("send")]
+        [HttpPost]
+        public async Task<IActionResult> SendNotification(NotificationModel notificationModel)
+        {
+            var result = await _notificationService.SendNotification(notificationModel);
+            return Ok(result);
+        }
+    }
+}
