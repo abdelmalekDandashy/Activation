8622b2dd6cbf94014f102b13f1c3c574c93248f1
abdelmalekDandashy <dandasheh1@gmail.com>
2022-02-02 23:27:43 +0400

    psuh

--- App/UnitTesting/Program.cs
+++ App/UnitTesting/Program.cs
@@ -30,50 +30,6 @@ namespace UnitTesting
             Tools.Tools oTools = new Tools.Tools();
             #endregion
 
-            WebRequest tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
-            tRequest.Method = "post";
-            //serverKey - Key from Firebase cloud messaging server  
-            tRequest.Headers.Add(string.Format("Authorization: key={0}", "AAAAhUx4cMY:APA91bHT3xtFC-Kwu9poDY2_R_CKNYVzKzJCEuASykdoanLKGxqo_L5Ku-Jl0nqDSOCKwYeCRnWj4dtMd480X8al-0TBnwla5dEIzfpu-wbcyZm-ZIUDwfGGRBKGESvXBpiAjgFuKhOW"));
-            //Sender Id - From firebase project setting  
-            tRequest.Headers.Add(string.Format("Sender: id={0}", "572513611974"));
-            tRequest.ContentType = "application/json";
-            var payload = new
-            {
-                to = "cCWE1c01Q-qT5GVYe9ee0f:APA91bGvrfUmjTFmx3srShHXZzDaLKwi9HX26_RklVgW1X6M7OE5Nw-YMErXXfsQzrmFrHR6k3uC-kL-dn5HoqfN90VZORXngfYZWCyCha1we9-iGSJRZsJv2mqvC05P1pu_NHFYTE_5",
-                priority = "high",
-                content_available = true,
-                notification = new
-                {
-                    body = "LAK byekloooooooooooooooo",
-                    title = "Wassim shubeh ?",
-                    badge = 1
-                },
-                data = new
-                {
-                    key1 = "value1",
-                    key2 = "value2"
-                }
-
-            };
-
-            string postbody = JsonConvert.SerializeObject(payload).ToString();
-            Byte[] byteArray = Encoding.UTF8.GetBytes(postbody);
-            tRequest.ContentLength = byteArray.Length;
-            using (Stream dataStream = tRequest.GetRequestStream())
-            {
-                dataStream.Write(byteArray, 0, byteArray.Length);
-                using (WebResponse tResponse = tRequest.GetResponse())
-                {
-                    using (Stream dataStreamResponse = tResponse.GetResponseStream())
-                    {
-                        if (dataStreamResponse != null) using (StreamReader tReader = new StreamReader(dataStreamResponse))
-                            {
-                                String sResponseFromServer = tReader.ReadToEnd();
-                                //result.Response = sResponseFromServer;
-                            }
-                    }
-                }
-            }
 
             //Params_Get_Extension_By_OWNER_ID oParams_Get_Extension_By_OWNER_ID  = new Params_Get_Extension_By_OWNER_ID();
 
@@ -100,24 +56,27 @@ namespace UnitTesting
             //Console.WriteLine(resulyt);
             //Console.WriteLine(resulyt);
 
+            for (var i = 0; i <= 10 ;i++)
+            {
+
+                Table oTable = new Table();
+                oTable.TABLE_ID = -1;
+                oTable.TABLE_NAME = "jan 6";
+                oTable.TABLE_AGE_COUNTER = 40;
+                oTable.IS_CHARGING = true;
+                oTable.CHARGING_PERCENTAGE = 50;
+                oTable.DEPO_ID = 1;
+                oTable.NB_OF_TYPE_A = 51;
+                oTable.NB_OF_TYPE_C = 50;
+                oTable.IS_READY = false;
+                oTable.OWNER_ID = 1;
+                oTable.ENTRY_USER_ID = 1;
+
+                oBLC.Edit_Tables(oTable);
+                Console.WriteLine(oTable.TABLE_ID);
+            }
 
 
-            //Table oTable = new Table();
-            //oTable.TABLE_ID = -1;
-            //oTable.TABLE_NAME = "jan 6";
-            //oTable.TABLE_AGE_COUNTER = 40;
-            //oTable.IS_CHARGING = true;
-            //oTable.CHARGING_PERCENTAGE = 100;
-            //oTable.DEPO_ID = 1;
-            //oTable.NB_OF_TYPE_A = 51;
-            //oTable.NB_OF_TYPE_C = 50;
-            //oTable.IS_READY = false;
-            //oTable.OWNER_ID = 1;
-            //oTable.ENTRY_USER_ID = 1;
-
-            //oBLC.Edit_Tables(oTable);
-
-            //Console.WriteLine(oTable);
 
 
 
--- App/WorkerService1/Worker.cs
+++ App/WorkerService1/Worker.cs
@@ -19,6 +19,63 @@ namespace WorkerService1
    
     public class Worker : BackgroundService
     {
+        #region sendNotification_METHOD
+
+
+        public void sendNotification(string i_notification)
+        {
+            #region send firebase notification
+            WebRequest tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
+            tRequest.Method = "post";
+            //serverKey - Key from Firebase cloud messaging server  
+            tRequest.Headers.Add(string.Format("Authorization: key={0}", "AAAAhUx4cMY:APA91bHT3xtFC-Kwu9poDY2_R_CKNYVzKzJCEuASykdoanLKGxqo_L5Ku-Jl0nqDSOCKwYeCRnWj4dtMd480X8al-0TBnwla5dEIzfpu-wbcyZm-ZIUDwfGGRBKGESvXBpiAjgFuKhOW"));
+            //Sender Id - From firebase project setting  
+            tRequest.Headers.Add(string.Format("Sender: id={0}", "572513611974"));
+            tRequest.ContentType = "application/json";
+            var payload = new
+            {
+                to = i_notification,
+                priority = "high",
+                content_available = true,
+                notification = new
+                {
+                    body = "wassim akalo men el loop",
+                    title = "Wassim shubeh ?",
+                    badge = 1
+                },
+                data = new
+                {
+                    key1 = "value1",
+                    key2 = "value2"
+                }
+
+            };
+
+            string postbody = JsonConvert.SerializeObject(payload).ToString();
+            Byte[] byteArray = Encoding.UTF8.GetBytes(postbody);
+            tRequest.ContentLength = byteArray.Length;
+            using (Stream dataStream = tRequest.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                using (WebResponse tResponse = tRequest.GetResponse())
+                {
+                    using (Stream dataStreamResponse = tResponse.GetResponseStream())
+                    {
+                        if (dataStreamResponse != null) using (StreamReader tReader = new StreamReader(dataStreamResponse))
+                            {
+                                String sResponseFromServer = tReader.ReadToEnd();
+                                //result.Response = sResponseFromServer;
+                            }
+                    }
+                }
+            }
+            //Console.WriteLine("more ways how to send push not from c#");
+            //Console.WriteLine("https://stackoverflow.com/questions/37412963/send-push-to-android-by-c-sharp-using-fcm-firebase-cloud-messaging");
+            //Console.WriteLine("3 seconds passed");
+            //await Task.Delay(3 * 1000, stoppingToken);
+            #endregion
+        }
+        #endregion
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
            
@@ -49,52 +106,53 @@ namespace WorkerService1
                 //Console.WriteLine(result.NUMBER_OF_EXTENSIONS);
 
 
+                Console.WriteLine("5 seconds passed");
+                await Task.Delay(5 * 1000, stoppingToken);
 
-         
                 var oParams_Get_Table_By_OWNER_ID = new Params_Get_Table_By_OWNER_ID() { OWNER_ID = 1 };
                 var resultTables = oBLC.Get_Table_By_OWNER_ID(oParams_Get_Table_By_OWNER_ID);
 
-                foreach(var table in resultTables)
-                {
+
+                //foreach(var table in resultTables)
+                //{
                     
 
-                    if(table.IS_CHARGING == true && table.CHARGING_PERCENTAGE <100)
-                    {
-                        var value = table.CHARGING_PERCENTAGE;
+                //    if(table.IS_CHARGING == true && table.CHARGING_PERCENTAGE <100)
+                //    {
+                //        var value = table.CHARGING_PERCENTAGE;
 
 
-                        switch (value)
-                        {
-                            case var expression when (value < 25):
-                                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 15;
-                                oBLC.Edit_Table(table);
-                                Console.WriteLine(table.CHARGING_PERCENTAGE+" is now " + table.TABLE_NAME);
-                                break;
+                //        switch (value)
+                //        {
+                //            case var expression when (value < 25):
+                //                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 15;
+                //                oBLC.Edit_Table(table);
+                //                Console.WriteLine(table.CHARGING_PERCENTAGE+" is now " + table.TABLE_NAME);
+                //                break;
 
-                            case var expression when (value < 50):
-                                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 12;
-                                oBLC.Edit_Table(table);
-                                Console.WriteLine(table.CHARGING_PERCENTAGE + " is now " + table.TABLE_NAME);
-                                break;
+                //            case var expression when (value < 50):
+                //                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 12;
+                //                oBLC.Edit_Table(table);
+                //                Console.WriteLine(table.CHARGING_PERCENTAGE + " is now " + table.TABLE_NAME);
+                //                break;
 
-                            case var expression when (value < 90):
-                                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 10;
-                                oBLC.Edit_Table(table);
-                                Console.WriteLine(table.CHARGING_PERCENTAGE + " is now " + table.TABLE_NAME);
-                                break;
+                //            case var expression when (value < 90):
+                //                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 10;
+                //                oBLC.Edit_Table(table);
+                //                Console.WriteLine(table.CHARGING_PERCENTAGE + " is now " + table.TABLE_NAME);
+                //                break;
 
-                            case var expression when (value < 98):
-                                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 3;
-                                oBLC.Edit_Table(table);
-                                Console.WriteLine(table.CHARGING_PERCENTAGE + " is now " + table.TABLE_NAME);
-                                break;
-                        }
+                //            case var expression when (value < 98):
+                //                table.CHARGING_PERCENTAGE = table.CHARGING_PERCENTAGE + 3;
+                //                oBLC.Edit_Table(table);
+                //                Console.WriteLine(table.CHARGING_PERCENTAGE + " is now " + table.TABLE_NAME);
+                //                break;
+                //        }
 
-                    }
-                }
+
+                //    }
+                //}
        
-                Console.WriteLine("5 seconds passed");
-                await Task.Delay(5*1000, stoppingToken);
 
 
             }
@@ -111,6 +169,7 @@ namespace WorkerService1
     public class Worker2 : BackgroundService
     {
 
+        #region sendNotification_METHOD
         public void sendNotification(string i_notification)
         {
             #region send firebase notification
@@ -164,6 +223,8 @@ namespace WorkerService1
             //await Task.Delay(3 * 1000, stoppingToken);
             #endregion
         }
+        #endregion
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
 
@@ -189,79 +250,51 @@ namespace WorkerService1
                 Tools.Tools oTools = new Tools.Tools();
                 #endregion
 
-                #region get firebase token
+                #region get firebase token and send low battery notification
 
                 var oParams_Get_User_By_OWNER_ID = new Params_Get_User_By_OWNER_ID() { OWNER_ID= 1};
                 var AllUsers = oBLC.Get_User_By_OWNER_ID(oParams_Get_User_By_OWNER_ID);
 
                 if(AllUsers != null && AllUsers.Count > 0)
-                {
+                {   
                     var tokenList = new List<string>();
-                    foreach(var token in AllUsers)
+
+                    #region check_tables_battery
+                    #region declaration
+                    var oParams_Get_Table_By_OWNER_ID = new Params_Get_Table_By_OWNER_ID() { OWNER_ID = 1 };
+                    var AllTablesList = oBLC.Get_Table_By_OWNER_ID(oParams_Get_Table_By_OWNER_ID);
+                    #endregion declaration
+                    if (AllTablesList != null && AllTablesList.Count > 0)
                     {
-                        if(token.FIREBASE_TOKEN != null)
+                        foreach (var table in AllTablesList)
                         {
-                            tokenList.Add(token.FIREBASE_TOKEN);
-                            sendNotification(token.FIREBASE_TOKEN);
-                            await Task.Delay(3 * 1000, stoppingToken);
+                            if (table.CHARGING_PERCENTAGE <= 20)
+                            {
+                                foreach (var user in AllUsers)
+                                {
+                                    if (user.FIREBASE_TOKEN != null)
+                                    {
+                                        //tokenList.Add(token.FIREBASE_TOKEN);
+                                        sendNotification(user.FIREBASE_TOKEN);
+                                        Console.WriteLine(table.CHARGING_PERCENTAGE+"battery %                              was sent to"+user.USERNAME);
+                                        //await Task.Delay(1 * 1000 *3600, stoppingToken);
+                                        
+                                    }
+                                }
+                            }
                         }
-                    }
+
+                        #endregion check_tables_battery
+                       
+
+                    }   //here close
                 }
 
 
                 #endregion
 
+                await Task.Delay(10 * 1000, stoppingToken);
 
-                //#region send firebase notification
-                //WebRequest tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
-                //tRequest.Method = "post";
-                ////serverKey - Key from Firebase cloud messaging server  
-                //tRequest.Headers.Add(string.Format("Authorization: key={0}", "AAAAhUx4cMY:APA91bHT3xtFC-Kwu9poDY2_R_CKNYVzKzJCEuASykdoanLKGxqo_L5Ku-Jl0nqDSOCKwYeCRnWj4dtMd480X8al-0TBnwla5dEIzfpu-wbcyZm-ZIUDwfGGRBKGESvXBpiAjgFuKhOW"));
-                ////Sender Id - From firebase project setting  
-                //tRequest.Headers.Add(string.Format("Sender: id={0}", "572513611974"));
-                //tRequest.ContentType = "application/json";
-                //var payload = new
-                //{
-                //    to = "cCWE1c01Q-qT5GVYe9ee0f:APA91bGvrfUmjTFmx3srShHXZzDaLKwi9HX26_RklVgW1X6M7OE5Nw-YMErXXfsQzrmFrHR6k3uC-kL-dn5HoqfN90VZORXngfYZWCyCha1we9-iGSJRZsJv2mqvC05P1pu_NHFYTE_5",
-                //    priority = "high",
-                //    content_available = true,
-                //    notification = new
-                //    {
-                //        body = "nafiii5",
-                //        title = "Wassim shubeh ?",
-                //        badge = 1
-                //    },
-                //    data = new
-                //    {
-                //        key1 = "value1",
-                //        key2 = "value2"
-                //    }
-
-                //};
-
-                //string postbody = JsonConvert.SerializeObject(payload).ToString();
-                //Byte[] byteArray = Encoding.UTF8.GetBytes(postbody);
-                //tRequest.ContentLength = byteArray.Length;
-                //using (Stream dataStream = tRequest.GetRequestStream())
-                //{
-                //    dataStream.Write(byteArray, 0, byteArray.Length);
-                //    using (WebResponse tResponse = tRequest.GetResponse())
-                //    {
-                //        using (Stream dataStreamResponse = tResponse.GetResponseStream())
-                //        {
-                //            if (dataStreamResponse != null) using (StreamReader tReader = new StreamReader(dataStreamResponse))
-                //                {
-                //                    String sResponseFromServer = tReader.ReadToEnd();
-                //                    //result.Response = sResponseFromServer;
-                //                }
-                //        }
-                //    }
-                //}
-                //Console.WriteLine("more ways how to send push not from c#");
-                //Console.WriteLine("https://stackoverflow.com/questions/37412963/send-push-to-android-by-c-sharp-using-fcm-firebase-cloud-messaging");
-                //Console.WriteLine("3 seconds passed");
-                //await Task.Delay(3 * 1000, stoppingToken);
-                ////#endregion
 
             }
         }
